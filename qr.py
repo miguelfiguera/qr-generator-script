79ddb2a215a6f6b02b2cf97cd4cb297c17ec36f8
Miguel Figuera <hola.digitallab@gmail.com>
2025-02-11 13:29:36 -0400

    qr generator initial commit and final

---
+++ qr.py
@@ -0,0 +1,20 @@
+import qrcode
+
+# URL you want to encode
+url = 'https://www.rradvisor.net'
+
+# Create a QR code object
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("qrcode.png")
